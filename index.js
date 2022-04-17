40acc08d3e8f2fb431c26ac82dc71dec0936e1d8
Andrii-Rodchenko <rod4enkoandriy@gmail.com>
2022-04-17 21:33:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -496,3 +496,21 @@
 //   }),
 // );
 // ```
+
+const bookShelf = {
+   books: ["The last kingdom", "Haze", "The guardian of dreams"],
+   
+  updateBook(oldName, newName) {
+    // Change code below this line
+
+    const oldBookIndex = this.books.indexOf(oldName);
+    
+    this.books.splice(oldBookIndex, 1, newName);
+
+    console.log(this.books) 
+
+    return this.books;
+     
+    // Change code above this line
+  },
+};
